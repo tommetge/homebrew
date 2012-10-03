aef78136c2836178ce6a1c1c6324a0e400da1410
tom metge <tom@accident-prone.com>
2012-10-03 15:27:04 -0600

    libtool: clang/libc++ support

--- Library/Formula/libtool.rb
+++ Library/Formula/libtool.rb
@@ -9,6 +9,13 @@ class Libtool < Formula
   mirror 'http://ftp.gnu.org/gnu/libtool/libtool-2.4.2.tar.gz'
   sha1 '22b71a8b5ce3ad86e1094e7285981cae10e6ff88'
 
+  head 'git://git.savannah.gnu.org/libtool.git', :tag => 'v2.4.2'
+
+  depends_on 'automake'
+  depends_on 'help2man'
+  depends_on 'xz'
+  depends_on 'autoconf'
+
   if MacOS::Xcode.provides_autotools? or File.file? "/usr/bin/glibtoolize"
     keg_only "Xcode (up to and including 4.2) provides (a rather old) Libtool."
   end
@@ -17,11 +24,20 @@ class Libtool < Formula
 
   def install
     ENV.universal_binary if build.universal?
+    if build.head?
+      system "./bootstrap"
+    end
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "--program-prefix=g",
                           "--enable-ltdl-install"
     system "make install"
+  end
+
+  def patches
+    if build.head?
+      'http://accident-prone.com/downloads/0001-Accept-clang-s-stdlib-linker-flag.patch'
+    end
   end
 
   def caveats; <<-EOS.undent
